ca71b95f5ae7abc1998bc34823abf449a8048f2b
Oriol_simon <orioltunino1234@gmail.com>
2023-05-22 02:55:51 -0500

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <v-app>
-      <v-main class="ma-0 pa-0">
+      <v-main class="ma-0 pa-0" v-if="!loader">
         <navbar v-if="verificar"/>
           <router-view></router-view>
           <topbtn />
         <pie v-if="verificar"/>
+      </v-main>
+      <v-main v-else>
+        <loader/>
       </v-main>
     </v-app>
   </div>
@@ -13,16 +16,19 @@
 <script>
  import navbar from './components/navbar.vue';
  import pie from './components/footer.vue';
- import topbtn from './components/topbtn.vue'
+ import topbtn from './components/topbtn.vue';
+ import loader from './components/loader.vue';
 export default{
   components:{
     navbar,
     pie,
     topbtn,
+    loader,
   },
   data(){
     return{
-      rutas:['/inicio','/nosotros',]//'/servicios','/contacto']
+      rutas:['/inicio','/nosotros',],//'/servicios','/contacto']
+      loader:true,
     }
 
   },
@@ -33,8 +39,13 @@ export default{
   },computed:{
     verificar(){
       return this.rutas.includes(this.$route.path)
-    }
-  },
+    },
+
+  },mounted(){
+    setTimeout(() => {
+            this.loader=false;
+    },1500);
+  }
 
 }
 </script>
---
+++ src/components/loader.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="d-flex" style="height: 100vh;">
+        <div class="loader">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name:'loader',
+}
+</script>
+<style >
+  body {
+  margin: 0;
+}
+
+.loader {
+  position: absolute;
+  top: 50%;
+  left: 40%;
+  margin-left: 10%;
+  transform: translate3d(-50%,-50%,0);
+}
+
+.dot {
+  width: 27px;
+  height: 27px;
+  background: #0d70b7;
+  border-radius: 100%;
+  display: inline-block;
+  animation: slide 1s infinite;
+}
+
+.dot:nth-child(1) {
+  animation-delay: 0.1s;
+  background: #0d70b7;
+}
+
+.dot:nth-child(2) {
+  animation-delay: 0.2s;
+  background:rgb(117, 179, 227);
+}
+
+.dot:nth-child(3) {
+  animation-delay: 0.3s;
+  background: #2b2c2c
+}
+
+.dot:nth-child(4) {
+  animation-delay: 0.4s;
+  background: rgb(230, 189, 135)
+}
+
+.dot:nth-child(5) {
+  animation-delay: 0.5s;
+  background: #ed9e35;
+}
+
+@keyframes slide {
+  0% {
+    transform: scale(1);
+  }
+  
+  50% {
+    opacity: 0.3;
+    transform: scale(2);
+  }
+  
+  100% {
+    transform: scale(1);
+  }
+}
+
+</style>
